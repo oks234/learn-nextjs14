5704487e3b63d98c457f2b5ab689e49b535dd49b
Kyungseok Oh <bestspx@gmail.com>
2024-11-19 12:48:35 +0900

    #4.3 Movie Trailers

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -10,7 +10,6 @@ export default async function MovieDetail({
   const { id } = await params;
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
