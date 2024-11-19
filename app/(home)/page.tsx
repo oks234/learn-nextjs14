a0f96d120af9a1af0625a70be279ef8304a39781
Kyungseok Oh <bestspx@gmail.com>
2024-11-19 10:23:29 +0900

    #3.2 Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,17 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      <h1>Hello</h1>
-      <p>{isLoading ? "loading..." : JSON.stringify(movies)}</p>
-    </div>
-  );
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
